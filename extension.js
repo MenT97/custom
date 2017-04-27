09fe26021234916620ca2a9b22f6ef87302abadf
MenT97 <misafisa@email.cz>
2017-04-27 18:18:35 +0200

    Update extension.js

--- extension.js
+++ extension.js
@@ -48,8 +48,8 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
       botName: ":<3: Žilinka :<3:",
       language: "english",
-      chatLink: "https://rawgit.com/basicBot/source/master/lang/en.json",
-      scriptLink: "https://rawgit.com/basicBot/source/master/basicBot.js",
+      chatLink: "https://rawgit.com/MenT97/source/master/lang/en.json",
+      scriptLink: "https://rawgit.com/MenT97/source/master/basicBot.js",
       roomLock: false, // Requires an extension to re-load the script
       startupCap: 1, // 1-200
       startupVolume: 0, // 0-100
@@ -112,6 +112,6 @@
     }));
 
     // Start the bot and extend it when it has loaded.
-    $.getScript("https://rawgit.com/basicBot/source/master/basicBot.js", extend);
+    $.getScript("https://rawgit.com/MenT97/source/master/basicBot.js", extend);
 
 }).call(this);
